5e730202e7d2c244d492af8fb1edb429382e218d
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-09 07:33:28 -0800

    can toggle state

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -3,22 +3,33 @@ import React, { Component } from "react";
 class Block extends Component {
   state = { displayTransaction: false };
 
+  toggleTransaction = () => {
+    this.setState({ displayTransaction: !this.state.displayTransaction });
+  };
+
+  get displayTransaction() {
+    const exampleString = "example";
+    return <div>Display Transaction: {exampleString}</div>;
+  }
 
   render() {
+    console.log("this.displayTransaction", this.displayTransaction);
     const { timestamp, hash, data } = this.props.block;
 
     const hashDisplay = `${hash.substring(0, 15)}...`;
     const stringifiedData = JSON.stringify(data);
 
-    const dataDisplay = stringifiedData.length > 35 ?
-         `${stringifiedData.substring(0, 35)}...` :
-         stringifiedData;
+    const dataDisplay =
+      stringifiedData.length > 35
+        ? `${stringifiedData.substring(0, 35)}...`
+        : stringifiedData;
 
     return (
       <div className="Block">
         <div>Hash: {hashDisplay} </div>
         <div>Timestamp: {new Date(timestamp).toLocaleString()} </div>
         <div>{dataDisplay}</div>
+        {this.displayTransaction}
       </div>
     );
   }
