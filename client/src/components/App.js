e094be19915625f85d4a6ae1b48251dec82f22ab
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-09 06:49:13 -0800

    add react components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import Blocks from "./Blocks";
 
 class App extends Component {
+  state = { walletInfo: {} };
+  componentDidMount() {
+    fetch("http://localhost:3000/api/wallet-info")
+      .then(response => response.json())
+      .then(json => this.setState({ walletInfo: json }));
+  }
   render() {
-    return <div>Unicoins</div>;
+    const { address, balance } = this.state.walletInfo;
+    return (
+      <div>
+        Unicoins
+        <div className="WalletInfo">
+          <h1>Address: {address}</h1>
+          <h1>Balance: {balance}</h1>
+        </div>
+        <br />
+        <Blocks />
+      </div>
+    );
   }
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<div>Unicoins in React</div>, document.getElementById("root"));
+
+render(<App />, document.getElementById("root"));
