14e300dedd167c346a42d3085c96621dfb2e3e47
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-09 12:48:20 -0800

    removing duplicate code in server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv").config();
 const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
+const isDevelopment = process.env.ENV === "development";
 
 const REDIS_URL = isDevelopment
   ? "redis://127.0.0.1:6379"
@@ -18,8 +19,6 @@ const PubSub = require("./app/pubsub");
 const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet");
 const TransactionMiner = require("./app/transaction-miner");
-
-const isDevelopment = process.env.ENV === "development";
 
 //create a new instance of blockchain,publisher/suscriber network, transcriber pool, wallet
 const blockchain = new Blockchain();
