73a8a80689a253af9638ddbea063d28bc786ad26
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-11 12:51:47 -0800

    troubleshooting

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -54,7 +54,7 @@ class Navbar extends Component {
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/transaction-pool">
-                Receipt
+                Updates
               </Link>
             </li>
           </ul>
