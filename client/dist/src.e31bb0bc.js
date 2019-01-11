96094afec763c3c4d4b41771083038c171203a1a
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-11 11:22:34 -0800

    update landing page

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47540,7 +47540,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54457" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51090" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
